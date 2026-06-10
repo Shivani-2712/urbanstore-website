a499d7ef44b1bee1cbc22137692b504f40fdeffd
Shivani-2712 <shivirana2712@gmail.com>
2026-06-10 23:45:35 +0530

    Redesign checkout page and improve cart UI

--- urban-store/src/pages/Checkout.jsx
+++ urban-store/src/pages/Checkout.jsx
@@ -206,103 +206,248 @@ function Checkout() {
     }
 
     return (
-        <div className="min-h-screen px-10 py-20">
-            <h1 className="text-5xl font-bold mb-10">
-                Checkout
-            </h1>
+        <div
+            className="
+    min-h-screen
+    px-10
+    py-16
+    bg-[#F8F4EE]
+    "
+        >
+            <div className="text-center mb-12">
 
-            <form className="max-w-2xl">
-                <input
-                    type="text"
-                    name="customerName"
-                    readOnly
-                    placeholder="Full Name"
-                    value={
-                        formData.customerName
-                    }
-                    onChange={handleChange}
-                    className="w-full border p-4 mb-4 rounded bg-gray-100"
-                />
-
-                <input
-                    type="email"
-                    name="email"
-                    readOnly
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="w-full border p-4 mb-4 rounded bg-gray-100"
-                />
-
-                <input
-                    type="text"
-                    name="phone"
-                    placeholder="Phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    className="w-full border p-4 mb-4 rounded"
-                />
-
-                <textarea
-                    name="address"
-                    placeholder="Address"
-                    value={formData.address}
-                    onChange={handleChange}
-                    className="w-full border p-4 mb-4 rounded"
-                />
-
-                <div className="mb-6">
-
-                    <div className="flex gap-3 mb-4">
-
-                        <input
-                            type="text"
-                            placeholder="Coupon Code"
-                            value={couponCode}
-                            onChange={(e) =>
-                                setCouponCode(
-                                    e.target.value
-                                )
-                            }
-                            className="border p-3 rounded w-full"
-                        />
-
-                        <button
-                            type="button"
-                            onClick={applyCoupon}
-                            className="bg-black text-white px-5 rounded"
-                        >
-                            Apply
-                        </button>
-
-                    </div>
-
-                    <h2 className="text-xl">
-                        Subtotal:
-                        ₹{totalAmount}
-                    </h2>
-
-                    <h2 className="text-xl text-green-600">
-                        Discount:
-                        ₹{discount}
-                    </h2>
-
-                    <h2 className="text-3xl font-bold">
-                        Final Total:
-                        ₹{finalAmount}
-                    </h2>
-
-                </div>
-
-                <button
-                    type="button"
-
-                    onClick={handlePayment}
-                    className="bg-black text-white px-8 py-4 rounded-lg"
+                <p
+                    className="
+        uppercase
+        tracking-[4px]
+        text-gray-400
+        mb-3
+        "
                 >
-                    Pay Now
-                </button>
-            </form>
+                    Secure Payment
+                </p>
+
+                <h1
+                    className="
+        text-5xl
+        font-serif
+        "
+                >
+                    Checkout
+                </h1>
+
+                <p className="text-gray-500 mt-3">
+                    {cartItems.length} Item(s)
+                </p>
+
+                <div className="w-24 h-px bg-[#D9CFC2] mx-auto mt-6 mb-8"></div>
+
+                <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-8">
+
+                    <div
+
+                        className="
+    bg-white
+    border
+    border-[#E8DCCB]
+    p-8
+    "
+                    >
+                        <p className="uppercase tracking-[4px] text-gray-400 text-sm mb-6">
+                            Customer Details
+                        </p>
+                        <form className="grid md:grid-cols-2 gap-6">
+                            <input
+                                type="text"
+                                name="customerName"
+                                readOnly
+                                placeholder="Full Name"
+                                value={
+                                    formData.customerName
+                                }
+                                onChange={handleChange}
+                                className="
+w-full
+border
+border-[#D9CFC2]
+p-4
+mb-4
+bg-[#F8F4EE]
+"
+                            />
+
+                            <input
+                                type="email"
+                                name="email"
+                                readOnly
+                                placeholder="Email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="
+w-full
+border
+border-[#D9CFC2]
+p-4
+mb-4
+bg-[#F8F4EE]
+"
+                            />
+
+                            <input
+                                type="text"
+                                name="phone"
+                                placeholder="Phone"
+                                value={formData.phone}
+                                onChange={handleChange}
+                                className="
+w-full
+border
+border-[#D9CFC2]
+p-4
+mb-4
+bg-white
+"
+                            />
+
+                            <textarea
+                                name="address"
+                                value={formData.address}
+                                onChange={handleChange}
+                                rows={3}
+                                className="
+    md:col-span-2
+    w-full
+    border
+    border-[#D9CFC2]
+    p-4
+    bg-white
+    "
+                            />
+                            <div className="md:col-span-2 mt-2">
+                                <div className="mb-6">
+
+                                    <div className="flex gap-3 mb-4">
+
+                                        <input
+                                            type="text"
+                                            placeholder="Coupon Code"
+                                            value={couponCode}
+                                            onChange={(e) =>
+                                                setCouponCode(
+                                                    e.target.value
+                                                )
+                                            }
+                                            className="border p-3 rounded flex-1 border-[#D9CFC2]"
+                                        />
+
+                                        <button
+                                            type="button"
+                                            onClick={applyCoupon}
+                                            className="bg-black text-white px-5 rounded"
+                                        >
+                                            Apply
+                                        </button>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div
+                        className="
+    bg-white
+    border
+    border-[#E8DCCB]
+    p-8
+    h-fit
+    "
+                    >
+                        <p className="uppercase tracking-[4px] text-gray-400 text-sm mb-4">
+                            Order Summary
+                        </p>
+                        <div className="mb-6">
+                            {cartItems.map((item) => (
+                                <div
+                                    key={item._id}
+                                    className="flex justify-between mb-2 text-sm"
+                                >
+                                    <span>
+                                        {item.name}
+                                        {item.size && ` (${item.size})`}
+                                        × {item.quantity}
+                                    </span>
+
+                                    <span>
+                                        ₹{item.price * item.quantity}
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="border-t border-[#E8DCCB] mb-6">
+                            <div className="flex justify-between mb-4">
+                                <span>Subtotal</span>
+                                <span>₹{totalAmount}</span>
+                            </div>
+
+                            <div className="flex justify-between mb-4 text-green-600">
+                                <span>Discount</span>
+                                <span>₹{discount.toFixed(0)}</span>
+                            </div>
+
+                            <div className="border-t border-[#E8DCCB] pt-6 mt-6">
+
+                                <div className="flex justify-between items-center">
+
+                                    <span
+                                        className="
+            uppercase
+            tracking-[4px]
+            text-gray-400
+            text-sm
+            "
+                                    >
+                                        Total
+                                    </span>
+
+                                    <span
+                                        className="
+            text-4xl
+            font-serif
+            "
+                                    >
+                                        ₹{finalAmount.toFixed(0)}
+                                    </span>
+
+                                </div>
+
+                            </div>
+                            <button
+                                type="button"
+
+                                onClick={handlePayment}
+                                className="
+w-full
+mt-8
+bg-black
+text-white
+py-5
+uppercase
+tracking-[4px]
+hover:opacity-90
+transition
+"
+                            >
+                                Pay Now
+                            </button>
+                            <p className="text-center text-xs text-gray-400 mt-5">
+                                Secure payments powered by Razorpay
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
